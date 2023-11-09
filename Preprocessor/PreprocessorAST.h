d6bc296038a5695e0810f23f05d333b2fa8a90d4
Mathias Vatter <mvatter@students.uni-mainz.de>
2023-11-09 14:22:34 +0100

    Add more PreNodes

--- Preprocessor/PreprocessorAST.h
+++ Preprocessor/PreprocessorAST.h
@@ -6,37 +6,50 @@
 
 #include <utility>
 
-#include "Tokenizer/Tokenizer.h"
+#include "../Tokenizer/Tokenizer.h"
 
 struct PreNodeAST {
-	Token token;
 	PreNodeAST* parent = nullptr;
-	explicit PreNodeAST(Token tok, PreNodeAST* parent= nullptr) : token(std::move(tok)), parent(parent) {}
+	explicit PreNodeAST(PreNodeAST* parent= nullptr) : parent(parent) {}
 };
 
 struct PreNodeNumber : PreNodeAST {
-	explicit PreNodeNumber(const Token &tok, PreNodeAST* parent= nullptr) : PreNodeAST(tok, parent) {}
+    Token number;
+	explicit PreNodeNumber(Token tok, PreNodeAST* parent= nullptr) : PreNodeAST(parent), number(std::move(tok)) {}
 };
 
 struct PreNodeKeyword : PreNodeAST {
-	explicit PreNodeKeyword(const Token &tok, PreNodeAST *parent) : PreNodeAST(tok, parent) {}
+    Token keyword;
+	explicit PreNodeKeyword(Token tok, PreNodeAST *parent) : PreNodeAST(parent), keyword(std::move(tok)) {}
 };
 
 struct PreNodeOther : PreNodeAST {
-	PreNodeOther(const Token &tok, PreNodeAST *parent) : PreNodeAST(tok, parent) {}
+    Token other;
+	PreNodeOther(Token tok, PreNodeAST *parent) : PreNodeAST(parent), other(std::move(tok)) {}
 };
 
 struct PreNodeChunk : PreNodeAST {
-	std::vector<PreNodeAST>
+	std::vector<PreNodeAST> chunk;
+    PreNodeChunk(std::vector<PreNodeAST> chunk, PreNodeAST *parent) : PreNodeAST(parent), chunk(std::move(chunk)) {}
 };
 
 struct PreNodeDefineHeader : PreNodeAST {
 	std::string name;
 	std::vector<std::vector<Token>> args;
-	PreNodeDefineHeader(std::string name, std::vector<std::vector<Token>> args, Token tok)
-		: NodeAST(tok), name(std::move(name)), args(std::move(args)) {}
-	NodeDefineHeader(const NodeDefineHeader& other);
-	std::unique_ptr<NodeAST> clone() const override;
+	PreNodeDefineHeader(std::string name, std::vector<std::vector<Token>> args, PreNodeAST *parent)
+		: PreNodeAST(parent), name(std::move(name)), args(std::move(args)) {}
+};
+
+struct PreNodeDefineStatement : PreNodeAST {
+    PreNodeDefineHeader header;
+    PreNodeChunk body;
+    explicit PreNodeDefineStatement(PreNodeDefineHeader header, PreNodeChunk body)
+    : header(std::move(header)), body(std::move(body)) {}
+};
+
+struct PreNodeDefineCall : PreNodeAST {
+    PreNodeDefineHeader define;
+    explicit PreNodeDefineCall(PreNodeDefineHeader define) : define(std::move(define)) {}
 };
 
 
