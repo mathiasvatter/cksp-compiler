dbd69f0a649b429239aca8cc0c6406d6d0131019
Mathias Vatter <mvatter@students.uni-mainz.de>
2024-01-21 22:35:21 +0100

    Update input files

--- main.cpp
+++ main.cpp
@@ -75,13 +75,13 @@ Options:
 //    input_filename = "/Users/mathias/Scripting/ro-ki/rho_des.ksp";
 //    input_filename = "/Users/mathias/Scripting/pipe-organ/pipe-organ.ksp";
 //    input_filename = "/Users/mathias/Scripting/preset-system/main.ksp";
-    input_filename = "/Users/Mathias/Scripting/action-woodwinds/action-ww.ksp";
+//    input_filename = "/Users/Mathias/Scripting/action-woodwinds/action-ww.ksp";
 //    input_filename = "/Users/Mathias/Scripting/horizon-leads/Horizon Leads.ksp";
 
 //    output_filename = "/Users/mathias/Scripting/the-score/Samples/Resources/scripts/the_score.txt";
 //    output_filename = "/Users/mathias/Scripting/the-score/Samples/Resources/scripts/the_score_cksp.txt";
 //    output_filename = "/Users/mathias/Scripting/preset-system/samples/resources/scripts/preset-system.txt";
-    output_filename = "/Users/mathias/Scripting/action-woodwinds/Samples/Resources/scripts/action_woodwinds.txt";
+//    output_filename = "/Users/mathias/Scripting/action-woodwinds/Samples/Resources/scripts/action_woodwinds.txt";
 
     std::cout << "Input File: " << input_filename << std::endl;
     std::cout << "Output File: " << output_filename << std::endl;
