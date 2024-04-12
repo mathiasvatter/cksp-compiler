494d9ada6521d8d87f7665ab203bfca8709a9f00
Mathias Vatter <mvatter@students.uni-mainz.de>
2024-04-12 21:56:25 +0200

    Update function substitution; Update executable

--- main.cpp
+++ main.cpp
@@ -25,9 +25,9 @@ int main(int argc, char* argv[]) {
     const std::string& standard_output_path = cli_options.get_standard_output_file();
 
 //    input_filename = "/Users/mathias/Scripting/sonu-libraries/main.ksp";
-//    input_filename = "C:\\Users\\mathi\\Documents\\Scripting\\the-score\\the-score.ksp";
+//    input_filename = R"(C:\Users\mathi\Documents\Scripting\the-score\the-score.ksp)";
 //    input_filename = "/Users/mathias/Scripting/the-score/the-score.ksp";
-    input_filename = "/Users/mathias/Scripting/time-textures/time-textures.ksp";
+//    input_filename = "/Users/mathias/Scripting/time-textures/time-textures.ksp";
 //    input_filename = "/Users/mathias/Scripting/legato-dev/legato.ksp";
 //    input_filename = "/Users/mathias/Scripting/ro-ki/rho_des.ksp";
 //    input_filename = "/Users/mathias/Scripting/pipe-organ/pipe-organ.ksp";
