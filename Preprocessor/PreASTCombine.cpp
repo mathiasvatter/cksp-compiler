12edf2a5adce93b51dfeb6dacd1c576b91b82797
Mathias Vatter <mvatter@students.uni-mainz.de>
2024-04-12 17:42:08 +0200

    no wtf time textures error was in .ksp itself

--- Preprocessor/PreASTCombine.cpp
+++ Preprocessor/PreASTCombine.cpp
@@ -14,9 +14,6 @@ void PreASTCombine::visit(PreNodeInt &node) {
 }
 
 void PreASTCombine::visit(PreNodeKeyword &node) {
-	if(node.keyword.val == "sli_layout031") {
-
-	}
     m_tokens.push_back(std::move(node.keyword));
 }
 
