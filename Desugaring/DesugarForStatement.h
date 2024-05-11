5aa1a52d113e7175305a67aa222bdbb4ac21dd5a
Mathias Vatter <mvatter@students.uni-mainz.de>
2024-05-11 15:31:54 +0200

    finish updating ASTDesugarStructs with reference nodes

--- Desugaring/DesugarForStatement.h
+++ Desugaring/DesugarForStatement.h
@@ -7,8 +7,8 @@
 #include "ASTDesugaring.h"
 
 /**
- * This class is responsible for desugaring for loops into while loops
- * and takes one for loop at a time to rewrite it as a
+ * @brief This class is responsible for desugaring for loops into while loops
+ * It takes one for loop at a time to rewrite it as a
  * while loop. Nested for loops need to be handled in the
  * parent ASTVisitor class.
  * The transformation is as follows:
@@ -33,45 +33,60 @@ public:
         std::unique_ptr<NodeAST> assign_var = iterator_var->clone();
         std::unique_ptr<NodeAST> function_var = iterator_var->clone();
 
-        // while statement
-        auto node_while_statement = std::make_unique<NodeWhileStatement>(node.tok);
         if(!node.step) {
             // function call
             std::string function_name = "inc";
             if (node.to.type == token::DOWNTO) function_name = "dec";
-            std::vector<std::unique_ptr<NodeAST>> args;
-            args.push_back(std::move(function_var));
-            auto inc_statement = make_function_call(function_name, std::move(args), node_while_statement.get(), node.tok);
-            inc_statement->update_parents(&node);
-            node.statements->statements.push_back(std::move(inc_statement));
+
+            auto args = std::make_unique<NodeParamList>(node.tok);
+            args->params.push_back(std::move(function_var));
+            auto node_inc = std::make_unique<NodeFunctionCall>(
+                    false,
+                    std::make_unique<NodeFunctionHeader>(
+                            "function_name",
+                            std::move(args), node.tok),
+                    node.tok
+            );
+            node.statements->statements.push_back(std::make_unique<NodeStatement>(std::move(node_inc), node.tok));
         } else {
-            auto assign_var2 = function_var->clone();
-            auto inc_expression = make_binary_expr(ASTType::Integer, token::ADD, std::move(function_var), std::move(node.step), &node, node.tok);
-            auto node_inc_statement = std::make_unique<NodeSingleAssignStatement>(std::move(assign_var2), std::move(inc_expression), node.tok);
-            auto node_statement = statement_wrapper(std::move(node_inc_statement), node_while_statement.get());
-            node_statement->update_parents(&node);
-            node.statements->statements.push_back(std::move(node_statement));
+            // i := i + step
+            auto inc_expression = std::make_unique<NodeBinaryExpr>(
+                    token::ADD,
+                    std::move(function_var),
+                    std::move(node.step), node.tok
+                    );
+            inc_expression->type = ASTType::Integer;
+            auto node_inc_statement = std::make_unique<NodeSingleAssignStatement>(
+                    std::move(function_var->clone()),
+                    std::move(inc_expression), node.tok);
+            node.statements->statements.push_back(std::make_unique<NodeStatement>(std::move(node_inc_statement), node.tok));
         }
 
         // handle while condition
         token comparison_op = token::LESS_EQUAL;
         if(node.to.type == token::DOWNTO) comparison_op = token::GREATER_EQUAL;
         // make comparison expression
-        auto comparison = make_binary_expr(ASTType::Comparison, comparison_op, std::move(iterator_var), std::move(node.iterator_end), node_while_statement.get(), node.tok);
+        auto comparison = std::make_unique<NodeBinaryExpr>(
+                comparison_op,
+                std::move(iterator_var),
+                std::move(node.iterator_end), node.tok
+                );
+        comparison->type = ASTType::Comparison;
 
-        node_while_statement->condition = std::move(comparison);
-        node_while_statement->statements = std::move(node.statements);
+        auto node_while_statement = std::make_unique<NodeWhileStatement>(
+                std::move(comparison),
+                std::move(node.statements), node.tok
+                );
 
-        auto node_assign_statement = std::make_unique<NodeSingleAssignStatement>(node.tok);
-        node_assign_statement->array_variable = std::move(assign_var);
-        node_assign_statement->assignee = std::move(node.iterator->assignee);
+        auto node_assign_statement = std::make_unique<NodeSingleAssignStatement>(
+                std::move(assign_var),
+                std::move(node.iterator->assignee),
+                node.tok
+                );
 
         auto node_body = std::make_unique<NodeBody>(node.tok);
-        node_body->statements.push_back(statement_wrapper(std::move(node_assign_statement), node_body.get()));
-
-        node_body->statements.push_back(statement_wrapper(std::move(node_while_statement), node_body.get()));
-        node_body->update_parents(node.parent);
+        node_body->statements.push_back(std::make_unique<NodeStatement>(std::move(node_assign_statement), node.tok));
+        node_body->statements.push_back(std::make_unique<NodeStatement>(std::move(node_while_statement), node.tok));
         replacement_node = std::move(node_body);
-//        node.replace_with(std::move(node_body));
     }
 };
