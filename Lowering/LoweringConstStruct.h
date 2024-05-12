4ba1fd9b1a70b7bd46477bc10f306357f085ebc4
Mathias Vatter <mvatter@students.uni-mainz.de>
2024-05-12 20:05:42 +0200

    Update LoweringConstStruct

--- Lowering/LoweringConstStruct.h
+++ Lowering/LoweringConstStruct.h
@@ -44,12 +44,17 @@ public:
         node.to_be_declared->accept(*this);
         // has no value -> create one
         if(!node.assignee) {
-            node.assignee = std::make_unique<NodeBinaryExpr>(token::ADD, m_pre->clone(), m_iter->clone(), node.tok);
+            node.assignee = std::make_unique<NodeBinaryExpr>(
+                    token::ADD,
+                    m_pre->clone(),
+                    m_iter->clone(), node.tok
+                    );
         } else {
             m_pre = node.assignee->clone();
-            m_iter = make_int(0, node.parent);
+            m_iter = std::make_unique<NodeInt>(0, node.tok);
         }
         node.to_be_declared->data_type = DataType::Const;
+        node.set_child_parents();
     };
 
     void visit(NodeConstStatement& node) override {
@@ -57,22 +62,39 @@ public:
         if(!m_const_prefixes.empty()) pref = m_const_prefixes.top() + "." + node.name;
         m_const_prefixes.push(pref);
         std::vector<std::unique_ptr<NodeAST>> const_indexes;
-        m_iter = make_int(0, node.parent);
-        m_pre = make_int(0, node.parent);
+        m_iter = std::make_unique<NodeInt>(0, node.tok);
+        m_pre = std::make_unique<NodeInt>(0, node.tok);
         for(int i = 0; i<node.constants->statements.size(); i++){
             node.constants->statements[i]->accept(*this);
-            const_indexes.push_back(std::make_unique<NodeBinaryExpr>(token::ADD, m_pre->clone(), m_iter->clone(), node.tok));
-            m_iter = make_binary_expr(ASTType::Integer, token::ADD, m_iter->clone(), make_int(1, node.parent), nullptr, node.tok);;
+            const_indexes.push_back(std::make_unique<NodeBinaryExpr>(
+                    token::ADD,
+                    m_pre->clone(),
+                    m_iter->clone(), node.tok));
+            m_iter = std::make_unique<NodeBinaryExpr>(
+                    token::ADD,
+                    m_iter->clone(),
+                    std::make_unique<NodeInt>(1, node.tok),
+                    node.tok
+                    );
         }
-        auto node_array = make_array(node.name, node.constants->statements.size(), node.tok, node.constants.get());
-        auto node_declare_statement = std::make_unique<NodeSingleDeclareStatement>(std::move(node_array), nullptr, node.tok);
-        node_declare_statement->assignee = std::unique_ptr<NodeParamList>(new NodeParamList(std::move(const_indexes), node.tok));
+        auto node_array = std::make_unique<NodeArray>(
+                std::nullopt,
+                node.name,
+                std::make_unique<NodeInt>(node.constants->statements.size(), node.tok),
+                node.tok
+                );
+        auto node_declare_statement = std::make_unique<NodeSingleDeclareStatement>(
+                std::move(node_array),
+                std::unique_ptr<NodeParamList>(new NodeParamList(std::move(const_indexes), node.tok)),
+                node.tok
+                );
         auto array = std::make_unique<NodeStatement>(std::move(node_declare_statement), node.tok);
-        array->update_parents(node.constants.get());
+//        array->update_parents(node.constants.get());
         node.constants->statements.push_back(std::move(array));
         auto constant = make_declare_variable(node.name+".SIZE", node.constants->statements.size(), DataType::Const, node.constants.get());
         node.constants->statements.push_back(std::move(constant));
-        node.constants->update_parents(node.parent);
+        node.constants->set_child_parents();
+//        node.constants->update_parents(node.parent);
 //        node.replace_with(std::move(node.constants));
         m_const_prefixes.pop();
     }
