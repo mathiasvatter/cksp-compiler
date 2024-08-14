bee12d6b032bd8cf179c72eb9b2533dfd926bda5
Mathias Vatter <mvatter@students.uni-mainz.de>
2024-08-14 19:21:07 +0200

    Apply ConstantPropagation class

--- AST/ASTVisitor/ASTOptimizations.h
+++ AST/ASTVisitor/ASTOptimizations.h
@@ -6,6 +6,7 @@
 
 #include "ASTVisitor.h"
 #include "../../Optimization/ConstantFolding.h"
+#include "../../Optimization/ConstantPropagation.h"
 
 /** @brief Class for AST Optimizations
  * Removing of unused variables, arrays, etc.
@@ -16,10 +17,52 @@ private:
 
 public:
     ASTOptimizations() = default;
+
+	inline NodeAST* visit(NodeProgram& node) override {
+		static ConstantPropagation constant_propagation;
+		node.accept(constant_propagation);
+
+		m_program = &node;
+		m_program->global_declarations->accept(*this);
+		for(auto & struct_def : node.struct_definitions) {
+			struct_def->accept(*this);
+		}
+		for(auto & callback : node.callbacks) {
+			callback->accept(*this);
+		}
+		for(auto & function_definition : node.function_definitions) {
+			function_definition->accept(*this);
+		}
+		node.merge_function_definitions();
+		node.reset_function_visited_flag();
+		return &node;
+	}
+
 	/// do constant folding for int and reals
-	NodeAST * visit(NodeBinaryExpr& node) override;
+	inline NodeAST* visit(NodeBinaryExpr& node) override {
+		static ConstantFolding constant_folding;
+		node.accept(constant_folding);
+		return &node;
+	}
+
 	/// remove unused variables
-	NodeAST * visit(NodeSingleDeclaration& node) override;
+	inline NodeAST* visit(NodeSingleDeclaration& node) override {
+		// remove unused variables -> do not remove UIControls
+		if(!node.variable->is_used and node.variable->get_node_type() != NodeType::UIControl) {
+			return node.replace_with(std::make_unique<NodeDeadCode>(node.tok));
+		}
+
+		node.variable->accept(*this);
+		if(node.value) node.value->accept(*this);
+		return &node;
+	}
+
     /// do node body cleanup
-	NodeAST * visit(NodeBlock& node) override;
+	inline NodeAST* visit(NodeBlock& node) override {
+		for(auto& statement : node.statements) {
+			statement->accept(*this);
+		}
+		node.flatten();
+		return &node;
+	}
 };
