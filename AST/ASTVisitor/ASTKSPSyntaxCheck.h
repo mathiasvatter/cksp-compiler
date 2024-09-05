9555960c8861791adbe81da4b2168ecd9055c947
Mathias Vatter <mvatter@students.uni-mainz.de>
2024-09-05 14:38:10 +0200

    Add new classes to tackle 'memory exhausted' error due to bison parsing stack overflow

--- AST/ASTVisitor/ASTKSPSyntaxCheck.h
+++ AST/ASTVisitor/ASTKSPSyntaxCheck.h
@@ -7,45 +7,25 @@
 #include "ASTVisitor.h"
 #include "../../SyntaxChecks/KSPPersistency.h"
 #include "../../SyntaxChecks/KSPDeclarations.h"
+#include "../../Optimization/MemoryExhaustedNesting.h"
 
 /**
  * Adds persistence functions to variables that are declared with a persistence keyword.
  * Is assigned value to a declaration statement is not constant, split the declaration into a declaration and an assignment statement.
  * CAREFUL: This visitor will mess up the declaration pointers of the references.
- *
- * infos on the max_block_lines:
- * // one param functions
- * define ASSIGNMENT_DECLARATION := 4992
- * define ZERO_PARAM_FUNC := 4994
- * define ONE_PARAM_FUNC := 4991
- * define TWO_PARAM_FUNC := 4989
- * define THREE_PARAM_FUNC := 4987
- * define FOUR_PARAM_FUNC := 4985
- * define FIVE_PARAM_FUNC := 4985
- * define NKS2_FUNC := 2495
- *
- * define ONE_TOKEN := 4993 // declarations, functions with no params
- * define TWO_TOKEN := 4991 // assigments, functions with one param
- * define THREE_TOKEN := 4989 // functions with two params
- * define FOUR_TOKEN := 4987
- * define FIVE_TOKEN := 4985 // if-statements with empty else statements and while statements
- * define SIX_TOKEN := 4984 // if statements with non-empty else blocks
- *
- * define pts_one_token := ONE_TOKEN/ONE_TOKEN 1
- * define pts_two_token := TWO_TOKEN/ONE_TOKEN 4993/4991
- * define pts_three_token := THREE_TOKEN/ONE_TOKEN 4993/4989
- * define pts_four_token := FOUR_TOKEN/ONE_TOKEN 4993/4987
- * define pts_five_token := FIVE_TOKEN/ONE_TOKEN 4993/4985
- * define pts_six_token := SIX_TOKEN/ONE_TOKEN 4993/4984
- * define pts_nks_token := ONE_TOKEN/NKS2_FUNC 4993/2495
+ * Does the following ksp vanilla specific syntax transformations and checks:
+ * - Adds persistence functions to variables that are declared with a persistence keyword.
+ * - Splits declaration and assignment statements if the assignment is not constant.
+ * - Checks if the maximum number of UI controls is exceeded.
+ * - Checks if the maximum number of array elements is exceeded.
+ * - Checks if the maximum number of statements in one block is exceeded (memory exhaustion error), caused
+ * 	 by the Bison parser stackoverflow -> nest block further to avoid this error
  */
 class ASTKSPSyntaxCheck: public ASTVisitor {
 private:
 	DefinitionProvider *m_def_provider;
 	std::unordered_map<std::string, int> m_ui_control_count;
 
-	static const int MAX_BLOCK_LINES = 4991;
-	int max_block_lines = MAX_BLOCK_LINES;
 	static const int MAX_UI_CONTROLS = 999;
 	static const int MAX_ARRAY_ELEMENTS = 1000000;
 
@@ -68,66 +48,14 @@ private:
 		}
 	}
 
-	bool check_line_count(NodeBlock* node, int line_count) {
-		if(line_count > max_block_lines) {
-			auto error = ASTVisitor::get_raw_compile_error(ErrorType::SyntaxError, *node);
-			error.m_message = "Maximum number of lines in block exceeded ("+std::to_string(max_block_lines)+"). This will probably prompt a 'memory exhausted' error in KSP.";
-			error.print();
-			return true;
-		}
-		return false;
-	}
-
-	std::vector<std::unique_ptr<NodeBlock>> split_blocks(NodeBlock &block) {
-		int new_max_block_lines = calc_adjusted_max_block_lines(block);
-		std::vector<std::unique_ptr<NodeBlock>> result_blocks;
-		auto it = block.statements.begin();
-		while (it != block.statements.end()) {
-			// Erstelle einen neuen Block mit vorreserviertem Speicherplatz
-			auto new_block = std::make_unique<NodeBlock>(block.tok);
-			new_block->statements.reserve(new_max_block_lines);
-
-			// Kopiere bis zu max_block_size Statements in den neuen Block
-			for (size_t i = 0; i < new_max_block_lines && it != block.statements.end(); ++i, ++it) {
-				new_block->add_stmt(std::move((*it)));
-			}
-
-			// Füge den neuen Block zum Ergebnis hinzu
-			result_blocks.push_back(std::move(new_block));
-		}
-		block.statements.clear();
-		return result_blocks;
-	}
-
-	/// calculate new max block size, since for every control flow statement created, the max block size inside them
-	/// shrinks by 3 stmts
-	inline int calc_adjusted_max_block_lines(NodeBlock& block) const {
-		int block_size = block.statements.size();
-		return max_block_lines - (block_size/max_block_lines*3)-3;
-//		return MAX_BLOCK_LINES - (block_size/MAX_BLOCK_LINES*4)-3;
-	}
-
-	std::unique_ptr<NodeBlock> get_block_of_if_stmts(std::vector<std::unique_ptr<NodeBlock>>& blocks) {
-		Token tok = blocks[0]->tok;
-		auto new_block = std::make_unique<NodeBlock>(tok);
-		auto true_expr = std::make_unique<NodeBinaryExpr>(token::EQUAL, std::make_unique<NodeInt>(1,tok), std::make_unique<NodeInt>(1,tok),tok);
-		true_expr->ty = TypeRegistry::Boolean;
-		for(auto& block : blocks) {
-			auto node_if = std::make_unique<NodeIf>(
-				true_expr->clone(),
-				std::move(block),
-				std::make_unique<NodeBlock>(tok),
-				tok
-			);
-			new_block->add_stmt(std::make_unique<NodeStatement>(std::move(node_if), tok));
-		}
-		return new_block;
-	}
 
 public:
-	explicit ASTKSPSyntaxCheck(DefinitionProvider *definition_provider) : m_def_provider(definition_provider) {
-		max_block_lines = MAX_BLOCK_LINES;
-	};
+	explicit ASTKSPSyntaxCheck(DefinitionProvider *definition_provider) : m_def_provider(definition_provider) {};
+
+	NodeAST* fix_memory_exhausted_error(NodeAST& node) {
+		MemoryExhaustedNesting memory_exhausted_nesting;
+		return node.accept(memory_exhausted_nesting);
+	}
 
 	NodeAST* visit(NodeSingleDeclaration& node) override {
 		node.variable->accept(*this);
@@ -143,15 +71,7 @@ public:
 		for(auto & stmt : node.statements) {
 			stmt->accept(*this);
 		}
-		if(cast_node<NodeCallback>(node.parent) == m_program->init_callback) {
-
-		}
 		node.flatten();
-		if(check_line_count(&node, node.statements.size())) {
-			auto blocks = split_blocks(node);
-			max_block_lines = MAX_BLOCK_LINES;
-			return node.replace_with(get_block_of_if_stmts(blocks));
-		}
 		return &node;
 	};
 
@@ -183,21 +103,6 @@ public:
 		node.params->accept(*this);
 		return &node;
 	}
-
-	NodeAST* visit(NodeIf& node) override {
-//		max_block_lines =-1 ;
-		node.condition->accept(*this);
-		node.if_body->accept(*this);
-		node.else_body->accept(*this);
-		return &node;
-	};
-
-	NodeAST* visit(NodeWhile& node) override {
-//		max_block_lines =-1;
-		node.condition->accept(*this);
-		node.body->accept(*this);
-		return &node;
-	};
 
 
 };
---
+++ Optimization/TokenCounter.h
@@ -0,0 +1,107 @@
+//
+// Created by Mathias Vatter on 05.09.24.
+//
+
+#pragma once
+
+#include "../AST/ASTVisitor/ASTVisitor.h"
+
+/**
+ * @class TokenCounter
+ * counts the amount of tokens in a given AST (bison tokens)
+ */
+class TokenCounter : public ASTVisitor {
+private:
+	int m_tokens = 0;
+public:
+
+	int get_tokens(NodeAST& node) {
+		m_tokens = 0;
+		node.accept(*this);
+		return m_tokens;
+	}
+
+	NodeAST * visit(NodeArrayRef& node) override {
+		if(node.index) {
+			node.index->accept(*this);
+			m_tokens += 1;
+		}
+		return &node;
+	}
+
+	NodeAST * visit(NodeArray& node) override {
+		node.size->accept(*this);
+		m_tokens += 1;
+		return &node;
+	}
+
+	NodeAST* visit(NodeBinaryExpr& node) override {
+		// if its the first binary expression, add 2 token
+		node.left->accept(*this);
+		node.right->accept(*this);
+		if(node.parent->get_node_type() != NodeType::BinaryExpr) {
+			m_tokens += 2;
+		}
+		return &node;
+	}
+
+	NodeAST* visit(NodeUnaryExpr& node) override {
+		// if its the first unary expression, add 1 token
+		node.operand->accept(*this);
+		// := -(1+1)
+		if(node.parent->get_node_type() == NodeType::SingleAssignment or node.parent->get_node_type() == NodeType::SingleDeclaration) {
+			m_tokens += 1;
+		}
+		return &node;
+	}
+
+	NodeAST* visit(NodeFunctionCall& node) override {
+		node.function->accept(*this);
+		int func_tokens = node.function->args->params.size();
+		func_tokens += 1; // because of function name
+		m_tokens += func_tokens;
+		if(contains(node.function->name, "nks")) {
+			m_tokens += func_tokens;
+		}
+		return &node;
+	}
+
+	NodeAST* visit(NodeSingleAssignment& node) override {
+		node.l_value->accept(*this);
+		node.r_value->accept(*this);
+		m_tokens += 2;
+		return &node;
+	}
+
+	NodeAST* visit(NodeSingleDeclaration& node) override {
+		node.variable->accept(*this);
+		m_tokens += 1; // because of variable name
+		if(node.value) {
+			node.value->accept(*this);
+			m_tokens += 1;
+		}
+		return &node;
+	}
+
+	NodeAST* visit(NodeUIControl& node) override {
+		node.control_var->accept(*this);
+		node.params->accept(*this);
+		m_tokens += 1; // because of ui_button keyword e.g.
+		m_tokens += node.params->params.size();
+		return &node;
+	}
+
+	NodeAST* visit(NodeIf& node) override {
+		m_tokens += 5; // if, condition, then, if, end
+		if(!node.else_body->statements.empty()) {
+			m_tokens += 1;
+		}
+		return &node;
+	}
+
+	NodeAST* visit(NodeWhile& node) override {
+		m_tokens += 5; // while, condition, while, end
+		return &node;
+	}
+
+};
