c276fcc390ed5d3e1036ae72848d937d2af2d139
Mathias Vatter <mvatter@students.uni-mainz.de>
2024-09-26 12:13:26 +0200

    Update windows executable again, remove setLocale command

--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@
 
 
 int main(int argc, char* argv[]) {
-	setlocale(LC_ALL, "C");
     CommandLineOptions cli_options(argc, argv);
 	auto config = cli_options.get_compiler_config();
 	Compiler compiler(config);
