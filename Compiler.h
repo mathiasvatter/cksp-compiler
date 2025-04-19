317626ed1dd79701c58672d613485858bf1a2c8e
Mathias Vatter <mathias.vatter@googlemail.com>
2025-04-19 22:58:27 +0200

    Update terminal output

--- Compiler.h
+++ Compiler.h
@@ -147,7 +147,7 @@ public:
 
 		compile_time.stop("Desugaring");
 		std::cout << compile_time.print_timer("Desugaring") << "\n";
-		compile_time.start("Variable Checking");
+		compile_time.start("Lexical Scope");
 
 		ASTTypeAnnotations type_annotations(m_program);
 		ast->accept(type_annotations);
@@ -157,8 +157,8 @@ public:
 		UniqueArrayNamesProvider unique_names_provider(m_program);
 		unique_names_provider.do_renaming(*m_program);
 
-		compile_time.stop("Variable Checking");
-		std::cout << compile_time.print_timer("Variable Checking") << "\n";
+		compile_time.stop("Lexical Scope");
+		std::cout << compile_time.print_timer("Lexical Scope") << "\n";
 		compile_time.start("Semantic Analysis");
 
 		ASTSemanticAnalysis data_structures(ast.get());
@@ -218,7 +218,7 @@ public:
 
 		compile_time.stop("Data Structure Lowering");
 		std::cout << compile_time.print_timer("Data Structure Lowering") << "\n";
-		compile_time.start("Variable Checking 1");
+		compile_time.start("Variable Checking");
 		ast->debug_print();
 
 		variable_checking.do_reachable_traversal(*ast, true);
@@ -226,8 +226,8 @@ public:
 		ast->collect_references();
 		infer_types.do_reachable_traversal(*ast);
 
-		compile_time.stop("Variable Checking 1");
-		std::cout << compile_time.print_timer("Variable Checking 1") << "\n";
+		compile_time.stop("Variable Checking");
+		std::cout << compile_time.print_timer("Variable Checking") << "\n";
 		compile_time.start("Global Scope");
 
 		// first pass to analyze dynamic extend within function definitions and replace with passive_vars
