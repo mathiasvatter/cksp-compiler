896f86d5997852fca46142fb839da41c2e7df0d4
Mathias Vatter <mvatter@students.uni-mainz.de>
2025-07-29 09:40:47 +0200

    Fix windows problem with inconsistent evaluation order of function arguments causing segfaults in windows builds :(

--- main.cpp
+++ main.cpp
@@ -7,7 +7,11 @@
 
 
 int main(int argc, char* argv[]) {
-	std::locale::global(std::locale("C"));
+	std::locale::global(std::locale::classic());
+	std::cout.imbue(std::locale::classic());
+	std::cin.imbue(std::locale::classic());
+
+	// std::locale::global(std::locale("C"));
     CommandLineOptions cli_options(argc, argv);
 	auto config = cli_options.get_compiler_config();
 	Compiler compiler(config);
