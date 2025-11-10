a7f037cb205b6bfd1f9a145c1a56dcbf734a7245
Mathias Vatter <mathias.vatter@googlemail.com>
2025-11-10 14:25:49 +0100

    Bump version to v0.0.8-alpha.5; Fix issue #14 where .txt files were flagged as unsupported file types

--- misc/FileHandler.h
+++ misc/FileHandler.h
@@ -7,6 +7,7 @@
 #include <filesystem>
 #include <unordered_map>
 #include "CompileError.h"
+#include "../utils/StringUtils.h"
 
 enum class FileType {
     ksp,
@@ -17,32 +18,38 @@ enum class FileType {
 };
 
 class FileHandler {
-public:
-    static std::string read_file(const std::string& filename);
-
-    explicit FileHandler(const std::string &mInputFile);    std::unordered_map<std::string, FileType> m_allowed_filetypes = {{".ksp", FileType::ksp}, {".cksp", FileType::cksp}, {".nckp", FileType::nckp}};
-    [[nodiscard]] const std::string &get_output() const;
-    [[nodiscard]] FileType get_file_type() const;
-    static FileType determine_file_type(const std::string& filepath) {
-        std::filesystem::path path(filepath);
-        std::string extension = path.extension().string();
-        if (extension == ".ksp") {
-            return FileType::ksp;
-        }
-        if (extension == ".cksp") {
-            return FileType::cksp;
-        }
-        if (extension == ".nckp") {
-            return FileType::nckp;
-        }
-        if (extension == ".txt") {
-            return FileType::txt;
-        }
-        return FileType::unknown;
-    }
-
-private:
+    std::unordered_map<std::string, FileType> m_allowed_filetypes = {
+        {".ksp", FileType::ksp},
+        {".cksp", FileType::cksp},
+        {".nckp", FileType::nckp},
+        {".txt", FileType::txt}
+    };
     std::string m_input_file;
     std::string m_output;
     FileType m_file_type;
+
+public:
+    explicit FileHandler(const std::string &mInputFile);
+    static std::string read_file(const std::string& filename);
+
+    [[nodiscard]] const std::string &get_output() const;
+    [[nodiscard]] FileType get_file_type() const;
+
+    FileType determine_valid_file_type(const std::string& filepath) {
+        const std::string extension = std::filesystem::path(filepath).extension().string();
+        const auto it = m_allowed_filetypes.find(extension);
+        if(it == m_allowed_filetypes.end()) {
+            auto allowed_types = std::string();
+            StringUtils::join_apply(
+                m_allowed_filetypes,
+                [](const auto& f) {
+                    return "*"+f.first;
+                },
+            ",");
+
+            CompileError(ErrorType::FileError, "Unable to open file. Not a valid file type.", -1, "*.ksp or *.nckp",extension, "").exit();
+            return FileType::unknown;
+        }
+        return it->second;
+    }
 };
